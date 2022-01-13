3a68366ba22ff7d20fe6dd09bfa405ff098a64fe
reona kasuya <reon.kasuya@gmail.com>
2022-01-13 20:41:15 +0900

    raw index page complete

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,17 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+  def show
+  end
+  def new
+  end
+  def edit
+  end
+  def create
+  end
+  def update
+  end
+  def destroy
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#destroy
+puts "Cleaning up database"
+Flat.destroy_all
+
+#seed
+puts "Populating database with Flats"
+
+100.times do
+  flat = Flat.create!(
+    name: Faker::Movies::LordOfTheRings.location,
+    address: Faker::Address.full_address,
+    description: Faker::Books::Dune.saying,
+    price_per_night: rand(5..500),
+    number_of_guests: rand(10..5000)
+  )
+  puts "Flat#{flat.id} has been created"
+end
+
+puts "done"
